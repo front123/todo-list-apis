1fa9f909e3f46b1ad6cb0547bcb1f2998fc3cf8e
LIANGFR2 <372334264@qq.com>
2020-08-06 20:45:10 +0800

    [todo-list-API][olivia,front]feature: pass should_return_2_items_when_get_todo_items_given_2_items_in_db

--- src/main/java/com/thoughtworks/springbootemployee/service/Impl/TodoListServiceImpl.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/Impl/TodoListServiceImpl.java
@@ -1,17 +1,18 @@
 package com.thoughtworks.springbootemployee.service.Impl;
-
 import com.thoughtworks.springbootemployee.entity.Item;
+import com.thoughtworks.springbootemployee.repository.TodoListRepository;
 import com.thoughtworks.springbootemployee.service.TodoListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class TodoListServiceImpl implements TodoListService {
-
+    @Autowired
+    private TodoListRepository todoListRepository;
     @Override
     public List<Item> getTodoItems() {
-        return new ArrayList<>();
+        return todoListRepository.findAll();
     }
 }
